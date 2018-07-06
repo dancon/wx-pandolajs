601c479688f5d9df864adfc8480154cc0c8f3a65
sizhao <870301137@qq.com>
2018-07-06 14:10:15 +0800

    feat: 使用 pandora-cli 初始化的小程序；

---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview 首页脚本
+ * @author sizhao | 870301137@qq.com
+ * @version 1.0.0 | 2018-06-25 | sizhao
+*/
+
+import { sayHello } from 'util/index.js'
+
+Page({
+  async onLoad () {
+    const str = await Promise.resolve(sayHello())
+    wx.showToast({
+      title: str
+    })
+  }
+})
